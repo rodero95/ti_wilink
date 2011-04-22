de5d1f97f0a6dab7d0783125e31387563b585be4
Alexander Hofbauer <alex@derhofbauer.at>
2011-04-22 11:06:16 +0200

    Update build script to take the Android source dir as paramter
    Prebuilt toolchain (exported to PATH) is used to compile the modules.

--- build_wlan.sh
+++ build_wlan.sh
@@ -1,5 +1,7 @@
-#Please modify KERNEL_DIR, BUILD_DIR
-export KERNEL_DIR=kernel_path
+ANDROID_DIR=$1
+
+export PATH="$ANDROID_DIR/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$ANDROID_DIR/out/host/linux-x86/bin/:$PATH"
+export KERNEL_DIR=$ANDROID_DIR/htc-kernel-msm7227
 
 export HOST_PLATFORM=msm
 export ARCH=arm
